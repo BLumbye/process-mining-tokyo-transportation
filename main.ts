74121910b1ea45c2027246de094564b3ab26b02c
Mads Harder <mads987a@gmail.com>
2025-10-29 11:49:03 +0100

    feat: create output dir to place files in

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 import { fetchAndSaveGtfsData } from "./gtfs";
+const path = require('bun:path');
+import {mkdir} from 'node:fs/promises';
+
+
 
 const RUN_EVERY_X_SECONDS = 5;
 
@@ -6,20 +10,23 @@ const dataObj = [
     {
         "resource": "https://api.odpt.org/api/v4/gtfs/realtime/ToeiBus",
         "baseFileName": "ToeiBus",
-    }
+    },
 ];
 
+// Create output directory if it doesn't exist
+await mkdir(process.env.OUTPUT_DIR!, { recursive: true });
 
 
 setInterval(() => {
     // log time
     for (const data of dataObj) {
         const baseUrl = data.resource;
-        const filePath = new Date().toISOString().split('T')[0] + `-${data.baseFileName}.jsonl`;
-        
-        fetchAndSaveGtfsData(baseUrl, filePath).then(() => {
-            console.log("test");
-        });
+        const currentDate = new Date().toISOString().split('T')[0];
+        const filePath = path.join(process.env.OUTPUT_DIR, currentDate + `-${data.baseFileName}.jsonl`)
+
+        // fetchAndSaveGtfsData(baseUrl, filePath).then(() => {
+        //     console.log("test");
+        // });
     }
 
 }, RUN_EVERY_X_SECONDS * 1000);
